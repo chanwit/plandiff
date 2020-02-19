baf411ecd527dfcbcf743c07d67b3af50076b4dc
Chanwit Kaewkasi <chanwit@gmail.com>
2020-02-19 12:49:21 +0700

    add plandiff

---
+++ plandiff.go
@@ -0,0 +1,15 @@
+package plandiff
+
+import (
+	"github.com/chanwit/plandiff/difflib"
+)
+
+func GetUnifiedDiff(currentPlan, newPlan string) (string, error) {
+	return difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
+		A:        difflib.SplitLines(currentPlan),
+		B:        difflib.SplitLines(newPlan),
+		FromFile: "current",
+		ToFile:   "new",
+		Context:  3,
+	})
+}
