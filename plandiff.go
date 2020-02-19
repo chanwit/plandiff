614c927aea1d5dba69e92b735e0596d5e6847a20
Chanwit Kaewkasi <chanwit@gmail.com>
2020-02-19 12:50:56 +0700

    add godoc comment

--- plandiff.go
+++ plandiff.go
@@ -4,6 +4,7 @@ import (
 	"github.com/chanwit/plandiff/difflib"
 )
 
+// GetUnifiedDiff generates a unified diff between the two WKSctl plans.
 func GetUnifiedDiff(currentPlan, newPlan string) (string, error) {
 	return difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
 		A:        difflib.SplitLines(currentPlan),
